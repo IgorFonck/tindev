51c9e947ef5cd48dfa15cdd6fabbe09078fbba0c
Igor Fonseca <igorfonsecaigor@gmail.com>
2019-08-08 15:49:47 -0300

    Criando estado para receber valor do formulário

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 
 import logo from '../assets/logo.svg';
 
-export default function Login() {
+export default function Login({ history }) {
+	const [username, setUsername] = useState('');
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		history.push('./main');
+	}
+
 	return (
 		<div className="login-container">
-			<form>
+			<form onSubmit={handleSubmit}>
 				<img src={logo} alt="Tindev" />
-				<input placeholder="Digite seu usuário do GitHub" />
+				<input 
+				placeholder="Digite seu usuário do GitHub" 
+				value={username}
+				onChange={e => setUsername(e.target.value)}
+				/>
 				<button type="submit">Enviar</button>
 			</form>
 		</div>
