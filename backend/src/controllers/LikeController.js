c45b4ab8ce968e111223b5ac8e1c2a348232d91d
Igor Fonseca <igorfonsecaigor@gmail.com>
2019-08-08 11:13:52 -0300

    Verificando match entre IDs

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -2,9 +2,7 @@ const Dev = require('../models/Dev');
 
 module.exports = {
 	async store(req, res) {
-		console.log(req.params.devId);
-		console.log(req.headers.user);
-
+		
 		const { user } = req.headers;
 		const { devId } = req.params;
 
@@ -15,6 +13,14 @@ module.exports = {
 			return res.status(400).json({ error: 'Dev not exists' });
 		}
 
-		return res.json({ ok:true });
+		if (targetDev.likes.includes(loggedDev._id)) {
+			console.log('DEU MATCH');
+		}
+
+		loggedDev.likes.push(targetDev._id);
+
+		await loggedDev.save();
+
+		return res.json(loggedDev);
 	}
 };
